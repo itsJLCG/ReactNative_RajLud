02a19ea908272ff53f7f3b0293d0c01a73bd31e1
RajTheGreatest <rajesh.respall@tup.edu.ph>
2025-03-28 16:12:28 +0800

    product detail fix

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,9 +1,17 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants/actionTypes';
 
-export const addToCart = (product) => ({
-  type: ADD_TO_CART,
-  payload: product,
-});
+export const addToCart = (product, quantity = 1) => {
+  // Make sure product has quantity property
+  const productWithQuantity = {
+    ...product,
+    quantity: product.quantity || quantity
+  };
+  
+  return {
+    type: ADD_TO_CART,
+    payload: productWithQuantity,
+  };
+};
 
 export const removeFromCart = (productId) => ({
   type: REMOVE_FROM_CART,
